1bd209fff5d496162492d7b497d3090b9f18d6f3
sarori <salo3678@gmail.com>
2019-12-24 22:51:52 +0100

    .

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({fav}){
-  return <h1>I like {fav}</h1>
-}
-
-function App() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <Food fav="kimchi" />
-      <Food fav="ramen" />
-      <Food fav="samgiopsal" />
-      <Food fav="chukumi" />
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+  render(){
+    return (
+      <div>
+      <h1>The number is: {this.state.count}</h1>
+      <button onClick={this.add}>Add</button>
+      <button onClick={this.minus}>Minus</button>
     </div>
-  );  
+    );
+  }
 }
 
 export default App;
