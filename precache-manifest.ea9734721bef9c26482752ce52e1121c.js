2d6b90197bde44d7b758a58c535d563b463064dd
sarori <salo3678@gmail.com>
2019-12-26 23:44:27 +0100

    Updates

---
+++ precache-manifest.ea9734721bef9c26482752ce52e1121c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1db2adb4f630665b76b8bf425c1f71d9",
+    "url": "/github.io/React_movieapp/index.html"
+  },
+  {
+    "revision": "fd3f7c65d11dbc0cc5c5",
+    "url": "/github.io/React_movieapp/static/css/main.a458bc84.chunk.css"
+  },
+  {
+    "revision": "bb974403da00fa96886c",
+    "url": "/github.io/React_movieapp/static/js/2.3196dca3.chunk.js"
+  },
+  {
+    "revision": "c6d01cb19213691bc81a274434d00fe7",
+    "url": "/github.io/React_movieapp/static/js/2.3196dca3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "fd3f7c65d11dbc0cc5c5",
+    "url": "/github.io/React_movieapp/static/js/main.78b231bc.chunk.js"
+  },
+  {
+    "revision": "18205167edca490d8207",
+    "url": "/github.io/React_movieapp/static/js/runtime-main.5a1aab9c.js"
+  }
+]);
